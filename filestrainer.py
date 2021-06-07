247c571b4532f453edda633056b5a81c3727e003
colorlion <godownfight@naver.com>
2021-06-07 17:26:10 +0900

    Fucking Envs

--- filestrainer.py
+++ filestrainer.py
@@ -1,2 +1,28 @@
 import pefile
 import elftools
+
+filename = "lsd"
+
+# 실행파일 체크
+def osCheck(filename):
+    file = open(filename, 'rb')
+    firstLine = file.readline()
+
+    if b'MZ' in firstLine:
+        winBitChecker(filename)
+        file.close()
+        print("windows")
+
+    if b'ELF' in firstLine:
+        file.close()
+        print("linux")
+
+def winBitChecker(filename):
+    pe = pefile.PE(filename, fast_load=True)
+
+    if hex(pe.FILE_HEADER.Machine) == '0x14c':
+        print("32bit") 
+    else:
+        print("64bit")
+
+osCheck(filename)
